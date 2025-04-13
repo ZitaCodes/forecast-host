b4e063abd8e2ec1353b6f39a6f169aae85bd8388
ZitaCodes <zitac85@gmail.com>
2025-04-13 19:27:06 -0400

    Update app.py
    fixed true last line code

--- app.py
+++ app.py
@@ -30,4 +30,7 @@ def reddit_forecast():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
