59c548fec7b96eac662aae1a15e88e1786f5c06d
ZitaCodes <zita@zitacodes.tech>
2025-04-13 20:03:43 -0400

    Add files via upload
    Enable CORS for Render access

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+
 from flask import Flask, jsonify
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/reddit-media")
 def reddit_forecast():
