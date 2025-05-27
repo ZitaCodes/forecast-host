0933c5b7b9e67283a8403c7889d183e6d007a91b
ZitaCodes <zitac85@gmail.com>
2025-05-27 18:30:42 -0400

    Create Pull.js
    To stop pull abuse by users on multiple devices

---
+++ models/Pull.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const pullSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  pulls_used: { type: Number, default: 0 },
+  pull_limit: { type: Number, default: 26 },
+  last_reset: { type: Date, default: () => new Date() }
+});
+
+module.exports = mongoose.model("Pull", pullSchema);
